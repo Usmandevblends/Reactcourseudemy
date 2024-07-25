222d1f8becf64f5ff740c427ab97e240c9979ff1
MuhammadUsman <imdevusman@gmail.com>
2024-07-25 16:31:48 +0500

    add new Pizza component with work

---
+++ 03-pizza-menu/pizza-app/src/components/Pizza.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Pizza() {
+  return (
+    <>  <img src="Pizzas/spinaci.jpg" alt="Pizza spinaci" />
+        <h2>Pizza Spinaci</h2>
+        <p>Tamoto/ mozarella, spinach, and ricotta cheese</p>
+    </>
+  )
+}
+
+export default Pizza
--- 03-pizza-menu/pizza-app/src/index.js
+++ 03-pizza-menu/pizza-app/src/index.js
@@ -1,24 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-
-function Pizza () {
-  return (
-    <>
-        <h1>pizza</h1>
-        <p>delicious</p>
-    </>
-  )
-}
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 
 
-
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />

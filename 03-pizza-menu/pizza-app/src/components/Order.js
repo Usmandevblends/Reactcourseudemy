32bf2216a7f3e0a84b4ad3ae6d22c532670a2a48
MuhammadUsman <imdevusman@gmail.com>
2024-07-29 15:57:25 +0500

    create order.js

---
+++ 03-pizza-menu/pizza-app/src/components/Order.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function order() {
+  return (
+    <div className="order">
+    <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+    <button className="btn">Order</button>
+  </div>
+  )
+}
+
+export default order

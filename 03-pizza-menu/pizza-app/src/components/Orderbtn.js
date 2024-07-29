3927d805fe39b3c33aa031dab14e6d58355cab06
MuhammadUsman <imdevusman@gmail.com>
2024-07-29 13:54:02 +0500

    create a contact alert

--- 03-pizza-menu/pizza-app/src/components/Orderbtn.js
+++ 03-pizza-menu/pizza-app/src/components/Orderbtn.js
@@ -1,12 +1,23 @@
 import React from "react";
 
 function Orderbtn(props) {
+  const handleOnClick = () => {
+    const alertBox = document.getElementById("alert-box");
+    alertBox.style.display = "block";
+    alertBox.innerHTML = "Order Now! Call us at +9203204168226";
+  };
+
   return (
     <div className="order">
       <p>
-        we're open until until {props.closeHours}:00. Come visit us or order online
+        we're open until until {props.closeHours}:00. Come visit us or order
+        online
       </p>
-      <button className="btn">Order</button>
+      <button className="btn" onClick={handleOnClick}>
+        Order
+      </button>
+      <div id="alert-box" className="alert-box">
+      </div>
     </div>
   );
 }

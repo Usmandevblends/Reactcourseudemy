2052b6608b12a7b47ec69d10dcfa8e8d42ab5680
MuhammadUsman <imdevusman@gmail.com>
2024-07-29 18:54:34 +0500

    add clicked funcation

--- 04-steps/steps/src/App.js
+++ 04-steps/steps/src/App.js
@@ -1,10 +1,49 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./style.css";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 
 function App() {
+  const step = 2;
+
+  function handlePrevious() {
+    alert("pervious");
+  }
+
+  function handleNext() {
+    alert("pervious");
+  }
+
   return (
-    <div className='steps'>App</div>
-  )
+    <div className="steps">
+      <div className="numbers">
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
+          Previous
+        </button>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;

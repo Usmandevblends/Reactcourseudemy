1189063a147184eaea4b5ad04b66ea5a39655b61
MuhammadUsman <imdevusman@gmail.com>
2024-07-29 12:38:54 +0500

    using props

--- 03-pizza-menu/pizza-app/src/components/Footer.js
+++ 03-pizza-menu/pizza-app/src/components/Footer.js
@@ -1,21 +1,23 @@
 import React from "react";
+import Orderbtn from "./Orderbtn";
 
 function Footer() {
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 20;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
 
   return (
     <footer className="footer">
-      {isOpen && (
-        <div className="order">
-        <p>We're open until {closeHour}:00. Come visit us or order online.</p>
-        <button className="btn">Order</button>
-        </div>
+      {isOpen ? (
+        <Orderbtn closeHour={openHour} />
+      ) : (
+        <p>
+          we're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
-  )
+  );
 }
 
 export default Footer;
---
+++ 03-pizza-menu/pizza-app/src/components/Orderbtn.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Orderbtn(props) {
+  return (
+    <div className="order">
+      <p>
+        we're open until until {props.closeHour}:00. Come visit us or order online
+      </p>
+      <button className="btn">Order</button>
+    </div>
+  );
+}
+
+export default Orderbtn;

e0f7d3d59d7d8e1c703ef57cd7af49b883b43a34
MuhammadUsman <imdevusman@gmail.com>
2024-08-02 18:47:48 +0500

    add some style

--- 04-steps/steps/src/Components/advice.js
+++ 04-steps/steps/src/Components/advice.js
@@ -10,23 +10,36 @@ function Advice() {
   function Increment() {
     setCount(count + 1);
   }
-
+  const date = new Date("8/2/2024");
+  date.setDate(date.getDate() + count);
   return (
-    <React.Fragment>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          color: "#fff",
-          gap: "10px",
-        }}
-      >
-        <button onClick={Decrement}>-</button>
-        <span>{count}</span>
-        <button onClick={Increment}>+</button>
+    <center>
+      <div className="card">
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: "15px",
+          }}
+        >
+          <button onClick={Decrement}>-</button>
+          <span>{count}</span>
+          <button onClick={Increment}>+</button>
+        </div>
+        <p>
+          <span>
+            {count === 0
+              ? "Today is "
+              : count > 0
+              ? `${count} days from today is `
+              : `${count} days ago was `}
+          </span>
+          <span>{date.toDateString()}</span>
+        </p>
       </div>
-    </React.Fragment>
+    </center>
   );
 }
 

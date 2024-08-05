02a4bee36562962a3a44eb45e393a0792ad94683
MuhammadUsman <imdevusman@gmail.com>
2024-08-05 12:03:38 +0500

    remove advice component

--- 04-steps/steps/src/Components/advice.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-
-function Advice() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(0);
-  return <></>;
-}
-
-export default Advice;

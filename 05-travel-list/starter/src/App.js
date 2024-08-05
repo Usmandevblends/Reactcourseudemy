55444bb9023b51bb7967f57cc27e36efb19ed6a7
MuhammadUsman <imdevusman@gmail.com>
2024-08-05 12:22:35 +0500

    travellist app setup

---
+++ 05-travel-list/starter/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+function App() {
+  return (
+    <div>
+       <logo />
+       <form  />
+    </div>
+  )
+}
+
+export default App
---
+++ 05-travel-list/starter/src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function logo() {
+  return (
+    <>
+          return <h1>🏝️ Far Away 🧳</h1>;
+
+    </>
+  )
+}
+
+export default logo

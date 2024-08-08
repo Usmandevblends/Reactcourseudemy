1d93c59d2d0b7a058facff61b81ceb2349ce0422
MuhammadUsman <imdevusman@gmail.com>
2024-08-08 18:43:46 +0500

    completed my cardflash project

--- 05-travel-list/card/src/components/FlashCard.js
+++ 05-travel-list/card/src/components/FlashCard.js
@@ -1,14 +1,18 @@
 import { useState } from "react";
 
 function FlashCard({ questions }) {
-  const [selectedId, setSelectedId] = useState(9103);
-
+  const [selectedId, setSelectedId] = useState(null);
+  function handleClick(id) {
+    setSelectedId(id !==  selectedId ? id: null );
+  }
   return (
     <div className="flashcards">
       {questions.map((question) => (
         <div
+          key={question.id}
+          onClick={() => handleClick(question.id)}
           className={question.id === selectedId ? "selected" : ""}
-          key={question.id}>
+        >
           <p>
             {question.id === selectedId ? question.answer : question.question}
           </p>

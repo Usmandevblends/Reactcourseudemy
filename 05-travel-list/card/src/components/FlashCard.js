dd248e9652905621585fe187c0ebfb340148495b
MuhammadUsman <imdevusman@gmail.com>
2024-08-08 14:04:00 +0500

    main

--- 05-travel-list/card/src/components/FlashCard.js
+++ 05-travel-list/card/src/components/FlashCard.js
@@ -1,11 +1,17 @@
-import React from "react";
+import { useState } from "react";
 
 function FlashCard({ questions }) {
+  const [selectedId, setSelectedId] = useState(9103);
+
   return (
     <div className="flashcards">
       {questions.map((question) => (
-        <div className="cards-content">
-          <p>{question.question}</p>
+        <div
+          className={question.id === selectedId ? "selected" : ""}
+          key={question.id}>
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
         </div>
       ))}
     </div>

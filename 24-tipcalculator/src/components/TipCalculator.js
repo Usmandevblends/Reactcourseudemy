53d5bd45689590d6145f81bb6ef5f265bd0008e0
MuhammadUsman <imdevusman@gmail.com>
2024-08-16 17:56:51 +0500

    Add a use state in TipCalculator

--- 24-tipcalculator/src/components/TipCalculator.js
+++ 24-tipcalculator/src/components/TipCalculator.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import BillInput from "./BillInput";
 import SelectPercentage from "./SelectPercentage";
 import Output from "./Output";
 import Reset from "./Reset";
 
 function TipCalculator() {
+  const [bill, setBill] = useState("");
+
   return (
     <div className="main">
-      <BillInput />
-      <SelectPercentage> How did you like the Service? </ SelectPercentage>
-      <SelectPercentage> How did your friend like the service? </ SelectPercentage>
+      <BillInput bill={bill} onSetBill={setBill} />
+      <SelectPercentage> How did you like the Service? </SelectPercentage>
+      <SelectPercentage>
+        {" "}
+        How did your friend like the service?{" "}
+      </SelectPercentage>
       <Output />
       <Reset />
     </div>

3c6891f78822e26a0c43c50f6318e85bc1a244d1
MuhammadUsman <imdevusman@gmail.com>
2024-08-26 11:23:22 +0500

    changes in app

--- 07-usepopcorn/usepopcorn/src/App.js
+++ 07-usepopcorn/usepopcorn/src/App.js
@@ -3,9 +3,7 @@ import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import { useState } from "react";
 
-
 const tempMovieData = [
-
   {
     imdbID: "tt1375666",
     Title: "Inception",
@@ -39,14 +37,13 @@ const tempMovieData = [
   },
 ];
 
-
-
 const tempWatchedData = [
   {
     imdbID: "tt1375666",
     Title: "Inception",
     Year: "2010",
-    Poster:"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
     runtime: 148,
     imdbRating: 8.8,
     userRating: 10,
@@ -68,7 +65,11 @@ function App() {
   return (
     <>
       <Navbar movies={movies} />
-      <Main tempMovieData={tempMovieData} tempWatchedData={tempWatchedData} movies={movies} />
+      <Main
+        tempMovieData={tempMovieData}
+        tempWatchedData={tempWatchedData}
+        movies={movies}
+      />
     </>
   );
 }
